701d122e7e329aa912aaf7d1a98843b388ecc1e0
cesarheladio <cesar_heladio@hotmail.com>
2024-04-26 12:54:58 -0600

    finish relation with states and cities

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -4,12 +4,14 @@ using Orders.Backend.Repositories.Implementations;
 using Orders.Backend.Repositories.Interfaces;
 using Orders.Backend.UnitsOfWork.Implementations;
 using Orders.Backend.UnitsOfWork.Interfaces;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +20,10 @@ builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnec
 builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
+builder.Services.AddScoped<ICountriesUnitOfWork, CountriesUnitOfWork>();
+builder.Services.AddScoped<IStatesRepository, StatesRepository>();
+builder.Services.AddScoped<IStatesUnitOfWork, StatesUnitOfWork>();
 
 
 var app = builder.Build();
--- Orders/Orders.Shared/Entities/City.cs
+++ Orders/Orders.Shared/Entities/City.cs
@@ -3,24 +3,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Orders.Shared.Entities
 {
-    public class State : IEntityWithName
+    public class City : IEntityWithName
     {
         public int Id { get; set; }
 
-        [Display(Name = "Estado / Departamento")]
+        [Display(Name = "Ciudad")]
         [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; } = null!;
 
-        public int CountryId { get; set; }
+        public int StateId { get; set; }
 
-        public Country? Country { get; set; }
-
-        public ICollection<City>? Cities { get; set; }
-
-        [Display(Name = "Ciudades")]
-        public int CitiesNumber => Cities == null || Cities.Count == 0 ? 0 : Cities.Count;
-
+        public State? State { get; set; }
     }
 
 }
